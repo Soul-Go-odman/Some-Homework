3d674afc3c0ae54eb1c2d418b5240e5ca229cd85
Soul-Go-odman <41051694+Soul-Go-odman@users.noreply.github.com>
2019-11-03 16:41:21 +0800

    Add files via upload

---
+++ Genetic_bagProgram/main_1.cpp
@@ -0,0 +1,399 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <iomanip>
+#include <random>
+#include <cassert>
+
+using namespace std;
+
+vector<vector<int>> datas = {  // goods datas
+		/*value volume weight*/
+		{8,  11, 22},
+		{9,  22, 15},
+		{15, 12, 4},
+		{6,  21, 5},
+		{16, 21, 10},
+		{9,  13, 19},
+		{1,  1,  21},
+		{4,  10, 20},
+
+		{14, 13, 8},
+		{9,  8,  13},
+		{3,  6,  2},
+		{7,  25, 3},
+		{12, 13, 6},
+		{4,  27, 17},
+		{15, 12, 12},
+		{5,  23, 5},
+
+		{18, 12, 12},
+		{5,  24, 4},
+		{15, 23, 12},
+		{4,  11, 21},
+		{6,  6,  14},
+		{2,  24, 23},
+		{12, 28, 17},
+		{14, 10, 15},
+
+		{11, 20, 20},
+		{9,  13, 22},
+		{13, 25, 25},
+		{13, 23, 0},
+		{14, 5,  22},
+		{13, 26, 15},
+		{19, 30, 25},
+		{4,  15, 13},
+};
+/*------------------Parameters------------------*/
+const int GOODS_COUNT = int(datas.size());
+int ITERATOR = 20;
+const long POPULATION_SIZE = 60;
+
+const int VOLUME_LIMIT = 80;
+const int WEIGHT_LIMIT = 80;
+/*--------------------------------------------*/
+
+
+class Goods {
+public:
+	Goods() : _value(0), _volume(0), _weight(0) {}
+
+	int get_value() { return _value; }
+	int get_volume() { return _volume; }
+	int get_weight() { return _weight; }
+
+	void set_value(int value) { _value = value; }
+	void set_volume(int volume) { _volume = volume; }
+	void set_weight(int weight) { _weight = weight; }
+
+private:
+	int _value;
+	int _volume;
+	int _weight;
+};
+
+
+class GA {  /*  Genetic Algorithm  */
+public:
+	GA(const int goods_count, const long population_size, const int iterator, const vector<Goods>& task) :
+		_chromosome_size(goods_count),
+		_population_size(population_size),
+		_iterator(iterator),
+		_task(task) {
+
+		int _sum_volume = 0, _sum_weight = 0, _sum_value = 0;
+		double pGene = 0;  // 随机生成的概率 0~1
+		const double pThreshold = 0.6;  // 概率阈值 大于该阈值置为1，否则为0
+
+		Chromosome _chromosome[60];
+		// Chromosome _chromosome[_population_size]; // Can't use in VC, but gcc
+		for (long i = 0; i < _population_size; ++i) {
+			_chromosome[i].set_num_gene(_chromosome_size);
+			_sum_value = 0, _sum_volume = 0, _sum_weight = 0;
+			for (int j = 0; j < _chromosome_size; j++) {
+				pGene = abs((double(rand()) / (double(RAND_MAX) + 1)));
+				_chromosome[i].set_gene(((pGene > pThreshold) ? 1 : 0));
+				
+				if (_chromosome[i].get_gene_item(j) == 1) {
+					_sum_volume += _task[j].get_volume();
+					_sum_weight += _task[j].get_weight();
+					_sum_value += _task[j].get_value();
+				}
+			}
+			
+			if ((_sum_volume > VOLUME_LIMIT) || (_sum_weight > WEIGHT_LIMIT)) {
+				_chromosome[i].empty_genes();
+				i--;
+				continue;
+			}
+
+			//cout << i << "  " << _sum_value << "  " << _sum_volume << "  " << _sum_weight << "  ";
+			//for (int j = 0; j < _chromosome_size; j++) {
+			//	cout << _chromosome[i].get_gene_item(j);
+			//	if ((j + 1) % 32 == 0) cout << endl;
+			//}
+			_population.push_back(_chromosome[i]);
+		}
+	}
+
+	double pCROSS = 0.8;  // 交叉概率
+	double pMUTATION = 0.1;  // 变异概率
+
+	class Chromosome {
+	public:
+		Chromosome() : _nums_gene(0), _fitness(0), selectProbability(0) {}
+
+		double selectProbability;
+
+		void set_num_gene(int chromosone_size) { _nums_gene = chromosone_size; }
+		void empty_genes() { vector<int>().swap(_gene); }
+
+		void set_gene(int gene) { _gene.push_back(gene); }
+		vector<int> get_gene() { return _gene; }
+
+		int get_gene_item(int index) { return _gene[index]; }
+		void set_gene_item(int index, int vaule) { _gene[index] = vaule; }
+
+		void set_fitness(int fitness) { _fitness = fitness; }
+		int get_fitness() { return _fitness; }
+
+		void set_constraint(int volume, int weight) {
+			vector<int>().swap(_constraint);  // empty
+			_constraint.push_back(volume);
+			_constraint.push_back(weight);
+		}
+		int get_constraint(string element) { 
+			if (element == "volume") {
+				return _constraint[0];
+			}
+			else if (element == "weight") {
+				return _constraint[1];
+			}
+			else {
+				assert("Wrong Parameter!");
+			}
+		}
+
+	private:
+		int _nums_gene;
+		vector<int> _gene;
+
+		int _fitness;
+		vector<int> _constraint;  // 约束：sum_Volume and sum_Weight
+	};
+
+	void fitness(vector<Chromosome>& population, vector<Goods>& _task) {
+		unsigned int value_sum = 0, volume_sum = 0, weight_sum = 0;
+		int gene_index = 0;
+		for (auto& chromosome : population) {
+			value_sum = 0, volume_sum = 0, weight_sum = 0;
+			gene_index = 0;
+			for (auto& gene : chromosome.get_gene()) {
+				if (gene) {
+					value_sum += _task[gene_index].get_value();
+					volume_sum += _task[gene_index].get_volume();
+					weight_sum += _task[gene_index].get_weight();
+				}
+				gene_index++;
+			}
+
+			/* Calculate fitness */
+			chromosome.set_fitness(value_sum);
+			chromosome.set_constraint(volume_sum, weight_sum);
+		}
+	}
+
+	void filter(vector<Chromosome>& population) {
+
+		long sum_fitness = 0;  // 计算概率值
+		for (auto& chromosome : population) {
+			sum_fitness += chromosome.get_fitness();
+		}
+		for (auto& chromosome : population) {
+			/* sum of probability is 1. */
+			chromosome.selectProbability = double(chromosome.get_fitness()) / double(sum_fitness);
+		}
+
+		double dropout;
+		double cumulativeProbability;  // 累计概率
+		long offset;
+
+		long num_dropout = long(0.4 * population.size());
+		for (long i = 0; i < num_dropout; ++i) {
+			dropout = abs((double(rand()) / (double(RAND_MAX) + 1)));  //随机选中的概率 0~1
+			cumulativeProbability = 0;
+			offset = 0;
+			for (auto& chromosome : population) {
+				cumulativeProbability += chromosome.selectProbability;
+				if ((chromosome.get_constraint("volume") > VOLUME_LIMIT) || 
+					(chromosome.get_constraint("weight") > WEIGHT_LIMIT) || 
+					(chromosome.get_fitness() < 0)) {
+					population.erase(population.begin() + offset);
+					break;
+				}
+				if (cumulativeProbability >= dropout) {
+					if (chromosome.get_fitness() > 80) {  //如果该个体的适应度(fitness)大于80则不进行筛选直接进入下一轮
+						i--;
+						break;
+					}
+
+					population.erase(population.begin() + offset);
+					break;
+				}
+				offset++;
+			}
+		}
+	}
+
+	void cross(vector<Chromosome>& population) {
+		int first = -1;
+		double corssProbability;
+		int index = 0;
+		int nGeneExchange = 0;  // 基因交换次数 1~3
+		int gene_index = 0;  // 交换基因的索引
+		int gene_temp = 0;  //  交换基因临时变量
+
+		vector<Chromosome> tempPopulation;  //TODO
+
+		for (auto& chromosome : population) {
+			corssProbability = abs((double(rand()) / (double(RAND_MAX) + 1)));
+			if (corssProbability < pCROSS) {
+				if (chromosome.get_fitness() > 80) {  //如果该个体的适应度(fitness)大于80则该染色体保留副本
+					tempPopulation.push_back(chromosome);
+				}
+
+				if (first < 0) first = index;
+				else {
+					nGeneExchange = int((rand() % 3) + 1);
+					for (int i = 0; i < nGeneExchange; i++) {
+						gene_index = int(rand() % chromosome.get_gene().size());
+						gene_temp = population[first].get_gene_item(gene_index);
+						population[first].set_gene_item(gene_index, population[index].get_gene_item(gene_index));
+						population[index].set_gene_item(gene_index, gene_temp);
+					}
+					first = -1;
+				}
+			}
+			index++;
+		}
+
+		for (auto& chromosome : tempPopulation) {
+			population.push_back(chromosome);
+		}
+	}
+
+	void mutation(vector<Chromosome>& population) {
+		double mutationProbability;  // 基因变异概率
+		int nMutation;  // 基因变异的数量
+		int mutationGene;  // 变异的基因
+
+		vector<Chromosome> tempPopulation;  // TODO
+
+		for (auto& chromosome : population) {
+
+			mutationProbability = abs((double(rand()) / (double(RAND_MAX) + 1)));
+			if (mutationProbability < pMUTATION) {
+				if (chromosome.get_fitness() > 80) {  //如果该个体的适应度(fitness)大于80则该染色体保留副本
+					tempPopulation.push_back(chromosome);
+				}
+
+				nMutation = rand() % chromosome.get_gene().size() + 1;
+				for (int i = 0; i < nMutation; i++) {
+					mutationGene = rand() % chromosome.get_gene().size();
+					chromosome.set_gene_item(mutationGene,
+						int(1 - chromosome.get_gene_item(mutationGene)));
+				}
+			}
+		}
+
+		for (auto& chromosome : tempPopulation) {
+			population.push_back(chromosome);
+		}
+	}
+
+	Chromosome get_best(vector<Chromosome>& population) {
+		Chromosome best;
+		for (auto& chromosome : population) {
+			//if ((chromosome.get_constraint("volume") < 81) && (chromosome.get_constraint("weight") < 81)) {
+				if (chromosome.get_fitness() > best.get_fitness()) {
+					best = chromosome;
+				}
+			//}
+		}
+		return best;
+	}
+
+	void print_result(Chromosome best, int index=-1) {
+		if (index >= 0) {
+			cout << right << setw(4) << index << " => ";
+		}
+
+		cout << "value: " << left << setw(6) << best.get_fitness();
+		cout << "volume: " << left << setw(6) << best.get_constraint("volume");
+		cout << "weight: " << left << setw(6) << best.get_constraint("weight");
+		cout << "chromosome: ";
+			int i = 1;
+			for (auto& gene : best.get_gene()) {
+				cout << gene;
+				if (i % 8 == 0) cout << ' ';
+				i++;
+			}
+			cout << endl;
+	}
+
+	void test(Chromosome test) {
+		int _value = test.get_fitness();
+		int _volume = test.get_constraint("volume");
+		int _weight = test.get_constraint("weight");
+	
+		int sumValue = 0, sumVolume = 0, sumWeight = 0;
+	
+		int gene;
+		for (int i = 0; i < test.get_gene().size(); i++) {
+			gene = test.get_gene_item(i);
+			if (gene) {
+				sumValue += _task[i].get_value();
+				sumVolume += _task[i].get_volume();
+				sumWeight += _task[i].get_weight();
+			}
+		}
+		cout << _value << "  " << _volume << "  " << _weight << endl;
+		cout << sumValue << "  " << sumVolume << "  " << sumWeight << endl;
+	}
+
+	void run() {
+		Chromosome best;
+		for (int i = 0; i < _iterator; i++) {
+			fitness(_population, _task);
+			filter(_population);
+			best = get_best(_population);
+			print_result(best, i);
+			Bestes.push_back(best);
+			cross(_population);
+			mutation(_population);
+		}
+		cout << "The best one" << endl;
+		print_result(get_best(Bestes));
+		//test(get_best(Bestes));
+
+	}
+
+private:
+	int _iterator;          // 迭代次数
+	int _chromosome_size;   // 物体总数
+	long _population_size;  // 种群数量
+	vector<Chromosome> _population; 
+	vector<Goods> _task;
+	vector<Chromosome> Bestes;
+};
+
+
+
+int main() {
+
+	/*-----------------Get 32 goods---------------*/
+	Goods goods[32];
+	// Goods goods[GOODS_COUNT];  // Can't use in VC, but gcc
+	vector<Goods> task;
+	int goods_index = 0;
+	for (auto& data : datas) {
+		goods[goods_index].set_value(data[0]);
+		goods[goods_index].set_volume(data[1]);
+		goods[goods_index].set_weight(data[2]);
+		task.push_back(goods[goods_index]);
+		goods_index++;
+	}
+	//int i = 0;
+	//for (auto& t : task) {
+	//	cout << i << "  " << t.get_value() << "  " << t.get_volume() << "  " << t.get_weight() << endl;
+	//	i++;
+	//}
+	/*--------------------------------------------*/
+
+	GA genetic(GOODS_COUNT, POPULATION_SIZE, ITERATOR, task);
+	genetic.run();
+
+	return 0;
+}
---
+++ Genetic_bagProgram/main_2.cpp
@@ -0,0 +1,313 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>      //stable_sort()函数的头文件
+#include <random>
+#include<ctime>
+
+using namespace std;
+
+#define  BAG_MAX_VOLUME	   80   //背包最大容积
+#define  BAG_MAX_WEIGHT    80	// 背包最大承重
+#define  MAX_NUM		   20 // 最优结果连续出现MAX_NUM次后，停止迭代
+
+//物品集合
+const int value[32] = { 8, 9, 15, 6, 16, 9, 1, 4, 14, 9, 3, 7, 12, 4, 15, 5, 18, 5, 15, 4, 6, 2, 12, 14, 11, 9, 13, 13, 14, 13, 19, 4 };
+const int volume[32] = { 11, 22, 12, 21, 21, 13, 1, 10, 13, 8, 6, 25, 13, 27, 12, 23, 12, 24, 23, 11, 6, 24, 28, 10, 20, 13, 25, 23, 5, 26, 30, 15 };
+const int weight[32] = { 22, 15, 4, 5, 10, 19, 21, 20, 8, 13, 2, 3, 6, 17, 12, 5, 12, 4, 12, 21, 14, 23, 17, 15, 20, 22, 25, 0, 22, 15, 25, 13 };
+
+//生成32位随机数
+mt19937 Rand(time(NULL));
+
+// 生成一个0-1之间的随机浮点数
+float Rand_Float()
+{
+	return (Rand() % 100 / 100.0);
+}
+
+class Item// 单件物品定义
+{
+public:
+	Item(int val, int vol, int wei) { value = val; volume = vol; weight = wei; }
+	int volume;		//体积
+	int weight;		// 重量
+	int value;		// 价值
+};
+
+vector<Item> goods; //存放物品的容器
+
+class Chromosome// 染色体
+{
+public:
+	Chromosome(int goods_num) { fitness = 0; sum_weight = 0; sum_volume = 0; generation_id = 0; InitGene(goods_num); }
+	Chromosome() { fitness = 0; sum_weight = 0; sum_volume = 0; generation_id = 0; gene.clear(); }
+	void InitGene(int goods_num) { gene.assign(goods_num, 0); }  //初始化染色体
+	int			fitness;		// 适应度
+	int			sum_weight;		// 总重量
+	int			sum_volume;		//总体积
+	int			generation_id;  // 当前遗传代数
+	vector<int>	gene;			// 存放基因的容器
+};
+
+
+bool ChromosomeSort(const Chromosome& one, const Chromosome& two)  //stable_sort函数中的排序模式
+{
+	return (one.fitness < two.fitness);
+}
+
+class GA// 遗传算法定义
+{
+public:
+	GA(int gn, float cr, float vr, int goods_n) : m_group_num(gn), m_cross_rate(cr), m_varia_rate(vr), m_goods_num(goods_n)
+	{
+		Init_Goods();
+	}
+
+	void Init_Goods()// 初始化物品集合
+	{
+		cout << "*********物品集合***********" << endl;
+		cout << "物品" << '\t' << "重量" << '\t' << "体积" << '\t' << "价值" << '\t' << endl;
+		for (int i = 0; i < m_goods_num; i++)
+		{
+			m_goods.push_back(goods[i]);
+			cout << i << '\t' << m_goods[i].weight << '\t' << m_goods[i].volume << '\t' << m_goods[i].value << endl;
+		}
+	}
+
+	static float TotalFitness(const vector<Chromosome>& group)//总适应度
+	{
+		float sum = 0.0;
+		for (size_t i = 0; i < group.size(); i++)
+		{
+			sum += group[i].fitness;
+		}
+		return sum;
+	}
+
+	void Operation()
+	{
+		int best_value_count = 0; //最优个体出现的次数
+		int max_fitness = 0;      //当前最大适应度
+		Init_Group();
+		for (int i = 0; best_value_count < MAX_NUM; i++)
+		{
+			max_fitness = m_best_Chromosome.fitness;
+			CalcFitness();         //适应度计算
+			RecordOptimalChromosome(i);// 记录最优个体
+			Select();
+			Cross();
+			Variation();
+
+			if (m_best_Chromosome.fitness == max_fitness)
+				best_value_count++;
+			else
+				best_value_count = 0;
+		}
+	}
+
+	void PrintOptimal()// 显示最优个体
+	{
+		cout << endl;
+		cout << "**************************最优解******************************" << endl;
+		cout << "最优解代数" << "\t" << "总重量" << "\t" << "总体积" << "\t" << "总价值" << "\t" << "物品选择" << endl;
+		cout << m_best_Chromosome.generation_id << "\t\t" << m_best_Chromosome.sum_weight << "\t" << m_best_Chromosome.sum_volume << "\t" << m_best_Chromosome.fitness << "\t";
+
+		int count = 0;
+		for (size_t i = 0; i < m_best_Chromosome.gene.size(); i++)
+		{
+			count++;
+			if (m_best_Chromosome.gene[i] == 1)
+				cout << count << " ";
+		}
+		cout << endl;
+		cout << endl;
+	}
+
+	void Init_Group()// 初始化群体
+	{
+		m_best_Chromosome.InitGene(m_goods_num);
+
+		int weight = 0, count = 0, volume = 0;
+
+		for (int i = 0; i < m_group_num; i++)
+		{
+			m_group.push_back(Chromosome(m_goods_num));
+			Chromosome& en = m_group.back();      //遗传群体容器中最上面一个染色体
+
+			// 产生一个0.5-1.0倍的承重值
+			float vir_capacity = ((Rand() % 50 + 50) / 100.0) * BAG_MAX_WEIGHT;
+
+			// 初始化该个体基因中的每一位
+			weight = 0;
+			count = 0;
+			while (weight <= vir_capacity)
+			{
+				int idx = Rand() % m_goods_num;
+
+				// 连续3次都随机到基因中的同一个染色体，则该个体初始化结束
+				if (count == 3) { break; }
+				if (en.gene[idx]) { count++; continue; }
+
+				en.gene[idx] = 1;
+				weight += m_goods[idx].weight;
+			}
+		}
+	}
+
+	void CalcFitness()// 适应度计算
+	{
+		for (size_t i = 0; i < m_group.size(); i++)
+		{
+			Chromosome& en = m_group[i];
+			int wei = 0, val = 0, vol = 0;
+			for (size_t j = 0; j < en.gene.size(); j++)
+			{
+				if (en.gene[j])
+				{
+					wei += m_goods[j].weight;
+					val += m_goods[j].value;
+					vol += m_goods[j].volume;
+				}
+			}
+			if (wei > BAG_MAX_WEIGHT) { en.fitness = 0; continue; }
+			if (vol > BAG_MAX_VOLUME) { en.fitness = 0; continue; }
+
+			en.fitness = val;
+			en.sum_weight = wei;
+			en.sum_volume = vol;
+		}
+	}
+
+	void RecordOptimalChromosome(int gid)// 记录最优个体
+	{
+		stable_sort(m_group.begin(), m_group.end(), ChromosomeSort);   //对群体中的每一个个体的适应度从小到大排序
+		Chromosome& en = m_group.back();                               //找出适应度最大的个体
+		if (en.fitness > m_best_Chromosome.fitness)
+		{
+			m_best_Chromosome = en;
+			m_best_Chromosome.generation_id = gid;
+		}
+
+
+		cout << gid << "\t\t" << en.sum_weight << "\t" << en.sum_volume << "\t" << en.fitness << "\t";
+		int count = 0;
+		for (size_t i = 0; i < en.gene.size(); i++)
+		{
+			count++;
+			if (en.gene[i] == 1)
+				cout << count << " ";
+		}
+		cout << endl;
+	}
+
+	void Select()// 选择
+	{
+		vector<Chromosome>  new_group;
+
+		stable_sort(m_group.begin(), m_group.end(), ChromosomeSort);
+
+		int src_group_num = m_group.size();
+
+		// 保留群体中最优的前10%
+		int reserve_num = (int)(src_group_num * 0.1);
+		for (int i = 0; i < reserve_num; i++)
+		{
+			new_group.push_back(m_group.back());
+			m_group.pop_back();
+		}
+
+		// 计算剩余个体的累积概率
+		vector<float>  selected_rate;
+		float sum_fitness = GA::TotalFitness(m_group);
+		selected_rate.push_back(m_group[0].fitness / sum_fitness);   //
+		for (size_t i = 1; i < m_group.size(); i++)
+		{
+			float cur_rate = selected_rate.back() + (m_group[i].fitness / sum_fitness);
+			selected_rate.push_back(cur_rate);
+		}
+
+		// 利用轮赌法选择剩下的40%
+		int left_num = (int)(src_group_num * 0.4);
+		for (int i = 0; i < left_num; i++)
+		{
+			float rand_rate = Rand_Float();
+			for (size_t idx = 0; idx < selected_rate.size(); idx++)
+			{
+				if (rand_rate <= selected_rate[idx])
+				{
+					new_group.push_back(m_group[idx]);
+					break;
+				}
+			}
+		}
+		m_group.clear();
+		m_group = new_group;// 新群体赋值
+	}
+
+	bool IsCross() { return (Rand_Float() <= m_cross_rate); }// 是否交叉	
+	void Cross()// 交叉
+	{
+		size_t src_group_num = m_group.size();
+
+		for (size_t i = 0; i < src_group_num - 1; i += 2)
+		{
+			Chromosome en1 = m_group[i];
+			Chromosome en2 = m_group[i + 1];
+
+			for (size_t j = 0; j < en1.gene.size(); j++)
+			{
+				if (IsCross())
+				{
+					int tmp = en1.gene[j];
+					en1.gene[j] = en2.gene[j];
+					en2.gene[j] = tmp;
+				}
+			}
+
+			m_group.push_back(en1);
+			m_group.push_back(en2);
+		}
+	}
+
+	bool IsVariation() { return (Rand_Float() <= m_varia_rate); }// 是否变异
+	void Variation()// 变异
+	{
+		for (size_t i = 0; i < m_group.size(); i++)
+		{
+			if (IsVariation())
+			{
+				Chromosome& en = m_group[i];
+				for (size_t j = 0; j < en.gene.size(); j++)
+				{
+					if (IsVariation())
+					{
+						en.gene[j] = (en.gene[j] ? 0 : 1);
+					}
+				}
+			}
+		}
+	}
+
+private:
+
+	int				    m_group_num;	// 群体数量
+	float				m_cross_rate;	// 交叉概率
+	float				m_varia_rate;	// 变异概率
+	int				    m_goods_num;	// 物品总数
+	Chromosome		    m_best_Chromosome;	// 最优个体
+	vector<Chromosome>	m_group;	// 遗传 - 群体
+	vector<Item>		m_goods;	// 总的物品集合
+
+};
+
+int main()
+{
+	for (int i = 0; i < 32; i++)
+		goods.push_back(Item(value[i], volume[i], weight[i]));
+	GA ga(1000, 0.5, 0.1, 32);
+	cout << endl << "*************************迭代过程******************************" << endl;
+	cout << "迭代次数" << "\t" << "总重量" << "\t" << "总体积" << "\t" << "总价值" << "\t" << "物品选择" << endl;
+	ga.Operation();
+	ga.PrintOptimal();
+
+	return 0;
+}
